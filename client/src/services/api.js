83db516837333596ebb52181f34efda08e3e2082
hasmithakre <hasmithakre1791@gmail.com>
2026-04-06 22:33:14 +0530

    Change in frontend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'process.env.REACT_APP_API_URL' });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
